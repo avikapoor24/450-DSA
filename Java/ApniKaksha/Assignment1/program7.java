02684b2956b09902d46b3469e577a8dd143cbba8
Avi Kapoor <avikapoor244@gmail.com>
2024-02-28 14:25:21 +0530

    Update program7.java

--- Java/ApniKaksha/Assignment1/program7.java
+++ Java/ApniKaksha/Assignment1/program7.java
@@ -1,12 +1,12 @@
 package Learning.Assignment1;
+
 import java.util.*;
 
 class program7 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Write a program to enter the numbers till the user wants " +
                 "and at the end it should display the count of positive, negative and " +
                 "zeros entered.");
-
 
     }
 }
